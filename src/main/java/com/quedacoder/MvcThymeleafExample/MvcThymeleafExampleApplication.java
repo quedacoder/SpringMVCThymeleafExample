c57a5728f15ccd4071318cb06cb6cab292839638
quedacoder <quedacoder@gmail.com>
2020-01-19 14:23:05 -0600

    initial project creation

---
+++ src/main/java/com/quedacoder/MvcThymeleafExample/MvcThymeleafExampleApplication.java
@@ -0,0 +1,13 @@
+package com.quedacoder.MvcThymeleafExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcThymeleafExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcThymeleafExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quedacoder/MvcThymeleafExample/MvcThymeleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quedacoder.MvcThymeleafExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcThymeleafExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
